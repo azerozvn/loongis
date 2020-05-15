1afccbbf0c6b2b97875a663d32a0a73cf6c1bb0c
Long Nguyen <nguoichet001@yahoo.com>
2020-05-15 15:07:23 -0700

    Converted front-end to react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TypeWriter from './components/TypeWriter';
+import SocialNetwork from './components/SocialNetwork';
 
 function App() {
+	const typeWriter = {
+		starter: 'I am a ',
+		wait: 2000,
+		words: [ 'Vietnamese', 'Web Developer', 'Content Creator' ]
+	};
+
+	const social = [
+		{
+			name: 'facebook',
+			prefix: 'fa',
+			full: 'fa-facebook-square',
+			url: 'https://www.facebook.com/azerozvn'
+		},
+		{
+			name: 'github',
+			prefix: 'fa',
+			full: 'fa-github',
+			url: 'https://github.com/azerozvn'
+		},
+		{
+			name: 'linkedin',
+			prefix: 'fa',
+			full: 'fa-linkedin',
+			url: 'https://www.linkedin.com/in/azerozvn/'
+		}
+	];
+
 	return (
 		<div className="App">
 			<Navbar />
+			<div className="container">
+				<div class="cover">
+					<h2>Hi, My name is Long.</h2>
+					<TypeWriter typeWriter={typeWriter} />
+					<div class="intro">
+						<p>
+							I'm living in Sacramento, CA and currently grinding my BS in Computer Science at Sac State.
+						</p>
+						<p>I weave webs and crunch bugs for food.</p>
+						<p>I love reddit, youtube and brain-hurting games.</p>
+						<p>I explore new tech, cook Asian food and cover love songs.</p>
+						<div class="container p-0">
+							<div class="row">
+								<div class="col-sm-6">
+									<p>Languages I fluent?</p>
+									<ul>
+										<li>Viet (douma)</li>
+										<li>English</li>
+										<li>PHP</li>
+										<li>JavaScript</li>
+									</ul>
+								</div>
+								<div class="col-sm-6">
+									<p>Tech I use?</p>
+									<ul>
+										<li>MEAN/MERN</li>
+										<li>Redux</li>
+										<li>NextJS</li>
+										<li>Agile</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<p>
+							Feeling cool? Reach me at <a href="mailto:longnb306@gmail.com">longnb306@gmail.com</a>
+						</p>
+						<div className="social-network">
+							{social.map((s, i) => {
+								return <SocialNetwork social={s} />;
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -1,19 +1,30 @@
-class TypeWriter {
-	constructor(txtElement, words, wait = 2000) {
-		this.txtElement = txtElement;
-		this.words = words;
+import React, { Component } from 'react';
+import './styles/TypeWriter.css';
+
+export class TypeWriter extends Component {
+	constructor(props) {
+		super(props);
+		this.txtElement = '';
 		this.txt = '';
 		this.wordIndex = 0;
-		this.wait = parseInt(wait, 10);
+		this.words = this.props.typeWriter.words;
+		this.wait = parseInt(this.props.typeWriter.wait, 10);
 		this.isDeleting = false;
 		this.typeSpeed = 400;
+		this.txtElement = <span className="txt-type" />;
 	}
+
+	componentDidMount() {
+		this.txtElement = document.querySelector('.txt-type');
+		this.type();
+	}
+
 	type = () => {
 		// Get current word
-		const fullTxt = this.words[this.wordIndex];
+		const fullTxt = this.words[this.wordIndex] + '.';
 		let typeSpeed = this.typeSpeed;
 
-		// Check Deleting
+		// Check deleting
 		if (this.isDeleting) {
 			if (this.txt === fullTxt) {
 				this.txtElement.style.borderRight = '0.2rem solid #bbb';
@@ -30,9 +41,9 @@ class TypeWriter {
 		// Update current word
 		this.txtElement.innerHTML = `${this.txt}`;
 
-		// Check word is finished
+		// Check if word is finished
 		if (!this.isDeleting && this.txt === fullTxt) {
-			// Add pause at end
+			// Add pause at the end
 			typeSpeed = this.wait;
 			// Remove writing cursor
 			this.txtElement.style.borderRight = 'none';
@@ -45,7 +56,7 @@ class TypeWriter {
 			this.isDeleting = false;
 			// Update word to type
 			if (this.wordIndex === this.words.length - 1) {
-				// Back to fist word
+				// Back to first word
 				this.wordIndex = 0;
 			} else {
 				// Move to next word
@@ -53,22 +64,20 @@ class TypeWriter {
 			}
 		}
 
-		// Continue type next word
+		// Type next word
 		setTimeout(() => this.type(), typeSpeed);
 	};
+
+	render() {
+		return (
+			<div>
+				<h1>
+					{this.props.typeWriter.starter}
+					<span className="txt-type" />
+				</h1>
+			</div>
+		);
+	}
 }
 
-// Type Method
-
-// Init on DOM Load
-document.addEventListener('DOMContentLoaded', init);
-// Init
-function init() {
-	// Get Parameters
-	const txtElement = document.querySelector('.txt-type');
-	const words = JSON.parse(txtElement.getAttribute('data-words'));
-	const wait = txtElement.getAttribute('data-wait');
-	// Init TypeWriter
-	const writer = new TypeWriter(txtElement, words, wait);
-	writer.type();
-}
+export default TypeWriter;
