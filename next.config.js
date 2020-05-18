88ad44285790b3810c472236187052b2a320432c
Long Nguyen <nguoichet001@yahoo.com>
2020-05-17 23:53:27 -0700

    Config for AWS Amplify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const withCSS = require('@zeit/next-css');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withCSS({
-	cssLoaderOptions: {
-		url: false
-	}
-});
+module.exports = withPlugins(
+	[
+		withCSS({
+			cssLoaderOptions: {
+				url: false
+			}
+		})
+	],
+	{ experimental: { css: true } }
+);
