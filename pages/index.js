497b3a9e222273b07b0d6221418f8c839aa2cc71
Long Nguyen <nguoichet001@yahoo.com>
2020-05-21 19:10:07 -0700

    Add lib and post static render method

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import Home from '../components/Home';
+import { getSortedPostData } from '../lib/post';
 
 export class HomePage extends Component {
+	constructor(props) {
+		super(props);
+		this.allPostData = this.props.allPostData;
+		console.log(this.allPostData);
+	}
+
 	render() {
 		const title = 'Loongis | Home';
 
 		return (
 			<Layout title={title}>
 				<Home />
+				<div className="container">
+					{this.allPostData.map((post) => {
+						return (
+							<p key={post.id}>
+								<b>
+									{post.title} - {post.date}
+								</b>
+							</p>
+						);
+					})}
+				</div>
 			</Layout>
 		);
 	}
+}
+
+export async function getStaticProps() {
+	const allPostData = getSortedPostData();
+	return {
+		props: {
+			allPostData
+		}
+	};
 }
 
 export default HomePage;
