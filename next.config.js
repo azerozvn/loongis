84b6ab2d888e349f59b933769b879201b78790cf
Long Nguyen <nguoichet001@yahoo.com>
2020-05-27 22:56:26 -0700

    Revise config and homepage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withPlugins = require('next-compose-plugins');
-const path = require('path');
 
 const nextConfig = {
 	experimental: { css: true }
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,17 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import Home from '../components/Home';
-import Link from 'next/link';
-import { getSortedPostData } from '../lib/post';
 
 export class HomePage extends Component {
-	constructor(props) {
-		super(props);
-		this.allPostData = this.props.allPostData;
-	}
-
 	render() {
 		const title = 'Loongis | Home';
 
 		return (
 			<Layout title={title}>
 				<Home />
-				<div className="container">
-					{this.allPostData.map((post) => {
-						return (
-							<div>
-								<Link href={'/post/' + post.id}>
-									<a key={post.id}>
-										<b>
-											{post.title} - {post.date}
-										</b>
-									</a>
-								</Link>
-							</div>
-						);
-					})}
-				</div>
 			</Layout>
 		);
 	}
-}
-
-export async function getStaticProps() {
-	const allPostData = getSortedPostData();
-	return {
-		props: {
-			allPostData
-		}
-	};
 }
 
 export default HomePage;
