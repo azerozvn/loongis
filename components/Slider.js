beba47166e1856cbec1dc50bfabec1a45cf96bad
Long Nguyen <nguoichet001@yahoo.com>
2020-05-27 22:55:45 -0700

    Update slider

--- components/Slider.js
+++ components/Slider.js
@@ -7,11 +7,31 @@ export class Slider extends Component {
 		this.sliderData = this.props.sliderData;
 	}
 
+	Quote() {
+		const quote = this.sliderData.quote;
+		return (
+			<div className="slider-quote">
+				<div className="container quote-box">
+					<h3 className="quote">"{quote.quote}"</h3>
+					<h4 className="author">&mdash; {quote.author}</h4>
+				</div>
+			</div>
+		);
+	}
+
 	render() {
+		let quote;
+		if (this.sliderData) {
+			if (this.sliderData.quote) {
+				quote = this.Quote();
+			}
+		}
+
 		return (
 			<div className="slider">
 				<div className="slider-veil" />
 				<img className="slider-img img-fluid" src="https://picsum.photos/3000/600" alt="slider-img" />
+				{quote}
 			</div>
 		);
 	}
