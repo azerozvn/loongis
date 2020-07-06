9d493caddfc970886ca8d74300d8fe7e9f93c85d
Zhany <nguoichet001@yahoo.com>
2020-07-05 19:14:14 -0700

    Add mdbreact

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import '../styles/Global.scss';
 
 // This default export is required in a new `pages/_app.js` file.
